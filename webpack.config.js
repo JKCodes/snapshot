5ef0788e105832500123b0adc46861621517b567
Joe Kim <josephkim0224@gmail.com>
2017-12-04 10:02:24 -0500

    create initial react component

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+var path = require('path');
+require('dotenv').config()
+
+module.exports = {
+  
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    filename: 'public/dist/bundle.js',
+    sourceMapFilename: 'public/dist/bundle.map'
+  },
+  devtool: '#source-map',
+  plugins: [
+    new webpack.EnvironmentPlugin([
+      'CLOUDINARY_CLOUDNAME',
+      'CLOUDINARY_API_KEY',
+      'CLOUDINARY_API_SECRET',
+      'CLOUDINARY_UPLOAD_PRESET'
+    ])
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
