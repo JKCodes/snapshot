f882b467e4ace05aed42a0a95c8045ad21342110
Joe Kim <josephkim0224@gmail.com>
2017-12-04 13:09:52 -0500

    adjusted spacing on Home layout

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react'
-import { Posts, MapNavigation } from '../containers'
+import { Posts, MapNavigation, Account } from '../containers'
 
 class Home extends Component {
-  render(){
-    return (
-      <div className="container">
-        Home Layout
-        <div className="row">
+	render(){
+		return (
+			<div className="container">
+				Home Layout
+				<div className="row">
 
-          <div className="col-md-3">
-            <MapNavigation />
+					<div className="col-md-3">
+						<MapNavigation />
 
-          </div>
+					</div>
 
-          <div className="col-md-6">
-            <Posts />
+					<div className="col-md-6">
+						<Posts />
 
-          </div>
+					</div>
 
-          <div className="col-md-3">
-            ACCOUNT
+					<div className="col-md-3">
+						<Account />
 
-          </div>
-        </div>
-      </div>
-    )
-  }
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default Home
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,6 +2,6 @@ import Home from './Home'
 
 export {
 
-  Home
-  
+	Home
+	
 }
