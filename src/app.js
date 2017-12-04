35909c5199bc00ffd9e6991a0021922c5dc08533
Joe Kim <josephkim0224@gmail.com>
2017-12-04 13:08:24 -0500

    rewrote the app.js to a simplied form

--- src/app.js
+++ src/app.js
@@ -4,16 +4,13 @@ import { Home } from './components/layout'
 import store from './stores'
 import { Provider } from 'react-redux'
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={ store.configureStore() }>
-        <div>
-          <Home />
-        </div>
-      </Provider>
-    )
-  }
-}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const app = (
+	<Provider store={ store.configureStore() }>
+		<div>
+			<Home />
+		</div>
+	</Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
