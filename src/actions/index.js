cf5d5999cb8bf816f361e9c5b5ae8a4b30ca5109
Joe Kim <josephkim0224@gmail.com>
2017-12-04 11:55:01 -0500

    add createPost and updatecurrentlocation

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,21 +2,50 @@ import constants from '../constants'
 import { APIManager } from '../utils'
 
 export default {
+
+  updateCurrentLocation: (location) => {
+    return {
+      type: constants.CURRENT_LOCATION_CHANGED,
+      location: location
+    }
+  },
+
+  createPost: (params) => {
+    return (dispatch) => {
+      APIManager
+      .post('/api/post', params)
+      .then(response => {
+
+      })
+      .catch((err) => {
+        console.log('ERROR: '+err)
+      })
+    }
+  },
+
   fetchPosts: (params) => {
     return (dispatch) => {
       APIManager
       .get('/api/post', null)
-      .then((response) => {
+      .then(response => {
         dispatch({
           type: constants.POSTS_RECEIVED,
           posts: response.results
         })
       })
       .catch((err) => {
-        console.log(err)
+        console.log('ERROR: '+err)
       })
-
-
     }
+
+  },
+
+  postsReceived: (posts) => {
+    return {
+      type: constants.POSTS_RECEIVED,
+      posts: posts
+    }
+
   }
+
 }
